784afbe0a9b8990e017a3f05e1716ac17d6bf5d6
Anbazhagan <anbublacky@gmail.com>
2016-10-20 18:58:14 +0530

    Devise added and User login / sign up integrated, with allowing name parameter in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
