cf2af3b9cc11a0d1a64776128ae95c13be629286
Anbazhagan <anbublacky@gmail.com>
2016-10-20 18:58:49 +0530

    slim generator initialization added

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module TaskManagement
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine :slim
+    end
   end
 end
