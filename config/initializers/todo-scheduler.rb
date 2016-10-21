5c18e80e5ba88e0074a56c8915c6d896786d9bfc
Anbazhagan <anbublacky@gmail.com>
2016-10-21 13:47:56 +0530

    scheduler changed to 1 day

--- config/initializers/todo-scheduler.rb
+++ config/initializers/todo-scheduler.rb
@@ -1,7 +1,7 @@
  require 'rufus/scheduler'
  scheduler = Rufus::Scheduler.new
 
- scheduler.every '1m' do
+ scheduler.every '1d' do
 
       puts "Test!"      
       system("rake todo_daily_list")	
