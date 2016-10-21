c02c42cf930c64bd84a45d4ff868b3a7674421c1
Anbazhagan <anbublacky@gmail.com>
2016-10-21 13:11:23 +0530

    production mailer added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'commutatus-task.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'herokuapp.com',
+    user_name:            'testudproduct@gmail.com',
+    password:             'unleashedblacklove',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
