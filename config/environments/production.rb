f464f792ffcb9c793ccae05094654718fa58bbf2
Anbazhagan <anbublacky@gmail.com>
2016-10-21 13:28:11 +0530

    mailer changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'commutatus-task.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'http://commutatus-task.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
