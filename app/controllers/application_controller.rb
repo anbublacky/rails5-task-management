d99d38f570ef02480b4b4f5c2905b347eb1aff88
Anbazhagan <anbublacky@gmail.com>
2016-10-22 17:36:52 +0530

    foreign key delete violation fix, form changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,16 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :rescue_from_fk_contraint, only: [:destroy]
+
+  def rescue_from_fk_contraint
+    begin
+      yield
+    rescue ActiveRecord::InvalidForeignKey
+    	redirect_to :back
+      flash[:alert] = 'You have associated records, so cant destroy'
+    end
+  end
 
   protected
 
